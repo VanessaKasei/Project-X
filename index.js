e33cab5e86490c2cc4182e72b65609325dbaa2fc
VanessaKasei <kaseivanessa@gmail.com>
2023-07-27 23:29:33 +0300

    function definition for buttons on the pictures

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ function toggleMenu() {
     var navLinks = document.getElementById("navLinks");
     navLinks.classList.toggle("show");
 }
+
+function readMore() {
+    document.getElementById("button1").innerHTML  = "red"
+}
+
+function eadMore() {
+    document.getElementById("button2").innerHTML  = "blue"
+}
+
+function adMore() {
+    document.getElementById("button3").innerHTML  = "yellow"
+}
